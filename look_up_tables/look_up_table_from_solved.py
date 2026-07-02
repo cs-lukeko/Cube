67a5d517279be3b161bbbd9175d57627b4021228
Luke Heath-Edwards <lukeheathedwards@gmail.com>
2026-07-02 23:10:39 +1200

    Altered moveset to enable states only reachable via EO moveset.

--- look_up_tables/look_up_table_from_solved.py
+++ look_up_tables/look_up_table_from_solved.py
@@ -1,5 +1,5 @@
 # Generates file with cube states n moves away from solved
-from constants import MOVES, AXES
+from constants import MOVES, AXES, EO_MOVESET
 from cube import Cube
 from pickle import dump
 from moves import apply_moves
@@ -8,7 +8,7 @@ def generate_look_up_table(n: int):
     database = {}
     depth_first_search(Cube(), n, [], database)
 
-    with open(f"database_{n}_away_from_solved.pkl", "wb") as file:
+    with open(f"look_up_tables/database_{n}_away_from_solved_eo.pkl", "wb") as file:
         dump(database, file)
 
     print(f"{len(database):,} states generated")
@@ -24,7 +24,7 @@ def depth_first_search(cube, depth_remaining, moves_so_far, database):
     if depth_remaining == 0:
         return None
 
-    for move in MOVES:
+    for move in EO_MOVESET:
         if len(moves_so_far) >= 1:
             if move[0] == moves_so_far[-1][0]:
                 continue
@@ -37,4 +37,4 @@ def depth_first_search(cube, depth_remaining, moves_so_far, database):
 
         depth_first_search(next_cube, depth_remaining - 1, moves_so_far + [move], database)
 
-generate_look_up_table(6)
+generate_look_up_table(8)
